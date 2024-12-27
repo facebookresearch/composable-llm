56ed66126027d39f2180bbcf4819e55b334ee0be
Vivien Cabannes <vivien.cabannes@gmail.com>
2024-12-27 11:03:49 +0000

    profiler - visualization

--- src/nanollama/monitor/profiler.py
+++ src/nanollama/monitor/profiler.py
@@ -1,8 +1,10 @@
+import csv
 import logging
 import time
 from dataclasses import dataclass
 from pathlib import Path, PosixPath
 
+import numpy as np
 import torch
 import torch.profiler as profiler
 
@@ -154,13 +156,61 @@ class LightProfiler:
         self.times = {}
         self.state = None
 
-    @staticmethod
-    def to_gib(memory):
-        return memory / (1024**3)
+    # ---------------------------------------------------------------------------
+    # Trace Visualization
+    # ---------------------------------------------------------------------------
+
+    @classmethod
+    def get_traces(cls, path: PosixPath) -> dict[int, dict[str, np.ndarray]]:
+        """
+        Get traces from csv files
+
+        Example
+        -------
+        ```python
+        from pathlib import Path
+        import matplotlib.pyplot as plt
+        from nanollama.monitor.profiler import LightProfiler
+
+        path = <your path to profiler traces>
+        res = LightProfiler.get_traces(path)
+        xlabel = 'step'
+        keys = list(res[0].keys())
+        for key in keys:
+            plt.figure()
+            for rank in res:
+                data = res[rank]
+                plt.plot(data[xlabel], data[key], label=f"rank {rank}")
+            plt.legend(); plt.title(key); plt.xlabel(xlabel)
+        ```
+        """
+        res = {}
+        for file_path in path.glob("*.csv"):
+            rank = int(str(file_path).split("_")[1])
+            header, data = cls.csv_to_numpy(file_path)
+            res[rank] = cls.process_data(header, data)
+        return res
 
     @staticmethod
-    def to_ratio(memory, device_capacity):
-        return memory / device_capacity
+    def csv_to_numpy(file_path: PosixPath) -> tuple[list[str], np.ndarray]:
+        with open(file_path, newline="") as csvfile:
+            csvreader = csv.reader(csvfile)
+            header = next(csvreader)
+            data = np.array([row for row in csvreader], dtype=float)
+        return header, data
+
+    @staticmethod
+    def process_data(header: list[str], data: np.ndarray) -> dict[str, np.ndarray]:
+        res = {}
+        index = -1
+        assert header[index] == "mem_capacity"
+        capacity = data[0, index]
+        for i, key in enumerate(header):
+            res[key] = data[:, i]
+            if key in ["mem", "mem_reserved"]:
+                res[key + "_gib"] = res[key] / (1024**3)
+                res[key + "_ratio"] = res[key] / capacity
+        return res
 
 
 # -------------------------------------------------------------------------------
