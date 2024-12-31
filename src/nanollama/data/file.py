ec3346c547fc8d05698751fcc986b6718486d39b
Vivien Cabannes <vivien.cabannes@gmail.com>
2024-12-31 14:57:19 +0000

    data from file - better logic

--- src/nanollama/data/file.py
+++ src/nanollama/data/file.py
@@ -21,23 +21,34 @@ def get_batch(nb_epochs: int, batch_size: int, path: str, n_data: int, rng: Gene
     batch_idx = np.array([], dtype=int)
     for _ in range(nb_epochs):
         epoch_idx = rng.permutation(n_data)
-        # add residual from the previous epoch
+        # add residual data from the previous epoch
         epoch_idx = np.append(batch_idx, epoch_idx)
 
         step = 0
         while True:
             begin, end = step * batch_size, (step + 1) * batch_size
             batch_idx = epoch_idx[begin:end]
+
+            # do not process last batch alone if it is smaller than batch_size
+            if batch_idx.shape[0] < batch_size:
+                break
+
+            # sort batch_idx to optimize I/O
             batch_idx.sort()
 
-            # remove potential duplicate between last batch of previous epoch and first batch of the new one
+            # remove potential duplicate when inserting residual batch
             if begin == 0:
-                batch_idx = np.unique(batch_idx)
+                batch_idx, duplicate = np.unique(batch_idx, return_counts=True)
+            else:
+                duplicate = None
 
             with h5py.File(path, "r") as f:
-                batch = f["data"][batch_idx]  # Load rows 2000 to 2999
+                batch = f["data"][batch_idx]
                 step += 1
-                yield batch
 
-            if batch.shape[0] < batch_size:
-                break
+            # handle duplicate
+            if duplicate and (duplicate != 1).any():
+                ind = np.repeat(np.arange(len(duplicate)), duplicate)
+                batch = batch[ind]
+
+            yield batch
