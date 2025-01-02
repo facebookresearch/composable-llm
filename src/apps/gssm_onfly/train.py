4c4c492fcb8342e1bfeb66d4c7e118be01241985
Vivien Cabannes <vivien.cabannes@gmail.com>
2025-01-02 12:50:44 +0000

    config= -> _

--- src/apps/gssm_onfly/train.py
+++ src/apps/gssm_onfly/train.py
@@ -283,7 +283,7 @@ def main() -> None:
 
     Usage:
     ```
-    python -m apps.my_app.train config=apps/my_app/configs/debug.yaml
+    python -m apps.my_app.train apps/my_app/configs/debug.yaml
     ```
     """
     import argparse
