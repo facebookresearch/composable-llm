3097087116d89aa4e210953d5f4b145b0ad20d97
Vivien Cabannes <vivien.cabannes@gmail.com>
2025-01-19 18:17:42 +0000

    mvp

--- exp_plots.py
+++ exp_plots.py
@@ -6,8 +6,6 @@ Report it against the number of parameters or the number of data.
 Plot scaling laws.
 """
 
-# %%
-
 from pathlib import Path
 from typing import Any
 
@@ -17,6 +15,8 @@ import yaml
 
 from nanollama.utils import flatten_config
 from nanollama.visualization import jsonl_to_numpy
+
+HOME_DIR = Path("/private/home/vivc/")
 
 
 def get_statistics(exp: int, names: list[str], nb_tasks: int, best: bool) -> dict[str, dict[str, dict[str, Any]]]:
@@ -41,7 +41,7 @@ def get_statistics(exp: int, names: list[str], nb_tasks: int, best: bool) -> dic
     all_res = {}
     for name in names:
         res = {}
-        log_dir = Path.home() / "logs" / f"exp{exp}" / name
+        log_dir = HOME_DIR / "logs" / f"exp{exp}" / name
 
         for task_id in range(nb_tasks):
             task_id += 1
@@ -50,22 +50,28 @@ def get_statistics(exp: int, names: list[str], nb_tasks: int, best: bool) -> dic
             # configuration
             with open(config_path) as f:
                 config = flatten_config(yaml.safe_load(f))
+
             data_seed = config["run_config.data.seed"]
-            model_seed = config["run_config.orchestration.utils.seed"]
+
+            # data configuration
+            onfly_config = True
             try:
-                nb_data = config["run_config.data.n_data"]
-            except KeyError:
-                nb_data = None
+                nodes = config["run_config.data.gssm.nodes"]
+            except AttributeError:
+                nodes = config["run_config.data.path"]
+                onfly_config = False
 
             metric_path = log_dir / "metrics" / str(task_id)
 
-            # number of parameters
-            try:
+            # scaling parameters
+            scaling_variable = None
+            if onfly_config:
+                # number of parameters when onfly
                 filepath = metric_path / "info_model.jsonl"
-                nb_params = jsonl_to_numpy(filepath, keys=["model_params"])["model_params"][0]
-            except FileNotFoundError:
-                nb_params = None
-                print("No model info", name, task_id)
+                scaling_variable = jsonl_to_numpy(filepath, keys=["model_params"])["model_params"][0]
+            else:
+                # number of data otherwise
+                scaling_variable = config["run_config.data.n_data"]
 
             # training results
             try:
@@ -75,7 +81,6 @@ def get_statistics(exp: int, names: list[str], nb_tasks: int, best: bool) -> dic
                     keys = ["loss", "step"]
                     data = jsonl_to_numpy(filepath, keys=keys)
                     rank_loss = data["loss"]
-                    assert data["step"][-1] == 10_000
                     if loss is None:
                         loss = rank_loss
                     else:
@@ -87,7 +92,9 @@ def get_statistics(exp: int, names: list[str], nb_tasks: int, best: bool) -> dic
                 best_train_loss = None
 
             # testing results
-            try:
+            if onfly_config:
+                best_test_loss = None
+            else:
                 loss = None
                 for rank in range(8):
                     filepath = metric_path / f"eval_{rank}.jsonl"
@@ -100,14 +107,11 @@ def get_statistics(exp: int, names: list[str], nb_tasks: int, best: bool) -> dic
                 loss /= 8
                 if best:
                     best_test_loss = loss.min()
-            except Exception:
-                best_test_loss = None
 
             res[task_id] = {
                 "data_seed": data_seed,
-                "model_seed": model_seed,
-                "nb_data": nb_data,
-                "nb_params": nb_params,
+                "nodes": nodes,
+                "scaling_var": scaling_variable,
                 "best_train_loss": best_train_loss,
                 "best_test_loss": best_test_loss,
             }
@@ -117,8 +121,8 @@ def get_statistics(exp: int, names: list[str], nb_tasks: int, best: bool) -> dic
 
 
 def extract_useful_info(exp: int, name: str) -> Any:
-    log_dir = Path.home() / "logs" / f"exp{exp}" / name
-    task_id = 2
+    log_dir = HOME_DIR / "logs" / f"exp{exp}" / name
+    task_id = 1
     config_path = log_dir / "tasks" / f"{task_id}.yaml"
     with open(config_path) as f:
         config = flatten_config(yaml.safe_load(f))
@@ -127,9 +131,7 @@ def extract_useful_info(exp: int, name: str) -> Any:
     data_seeds = config.get("launcher.grid.data.seed", None)
     if data_seeds is None:
         data_seeds = [config.get("run_config.data.seed")]
-    model_seeds = config.get("launcher.grid.orchestration.utils.seed", None)
-    if model_seeds is None:
-        model_seeds = [config.get("run_config.orchestration.utils.seed")]
+    graphs = config.get("launcher.grid.data.gssm.nodes", None)
 
     # wether training with finite data
     if "run_config.data.path" in config:
@@ -140,29 +142,24 @@ def extract_useful_info(exp: int, name: str) -> Any:
         xkey = "nb_params"
         ykey = "best_train_loss"
 
-    return xkey, ykey, data_seeds, model_seeds
+    return xkey, ykey, data_seeds, graphs
 
-
-# %%
 
 exp_dict = {
     1: [
-        {"names": ["small_X", "small_Z"], "nb_tasks": 72},
-        {"names": ["onfly_small_X", "onfly_small_Z"], "nb_tasks": 64},
+        {"names": ["onfly"], "nb_tasks": 234},
     ],
     2: [
-        {"names": ["one_node", "two_nodes", "four_nodes", "eight_nodes"], "nb_tasks": 72},
-        {"names": ["onfly_one_node", "onfly_two_nodes", "onfly_four_nodes", "onfly_eight_nodes"], "nb_tasks": 64},
+        {"names": ["onfly"], "nb_tasks": 72},
     ],
     3: [
-        {"names": ["easy", "medium", "hard", "dense"], "nb_tasks": 72},
-        {"names": ["onfly_easy", "onfly_medium", "onfly_hard", "onfly_dense"], "nb_tasks": 64},
+        {"names": ["onfly"], "nb_tasks": 72},
     ],
     4: [
-        {"names": ["base", "slow", "dead", "context"], "nb_tasks": 72},
-        {"names": ["onfly_base", "onfly_slow", "onfly_dead", "onfly_context"], "nb_tasks": 64},
+        {"names": ["onfly"], "nb_tasks": 72},
     ],
 }
+
 
 for exp in range(4):
     exp += 1
@@ -172,18 +169,18 @@ for exp in range(4):
 
         all_res = get_statistics(exp, names, nb_tasks, best=True)
 
-        xkey, ykey, data_seeds, model_seeds = extract_useful_info(exp, names[0])
+        xkey, ykey, data_seeds, all_nodes = extract_useful_info(exp, names[0])
 
         for data_seed in data_seeds:
-            for model_seed in model_seeds:
+            for nodes in all_nodes:
                 fig, ax = plt.subplots(1, 1, figsize=(6, 6))
                 for i, name in enumerate(names):
                     res = all_res[name]
                     xaxis = []
                     yaxis = []
                     for local_res in res.values():
-                        if local_res["model_seed"] == model_seed and local_res["data_seed"] == data_seed:
-                            xaxis.append(local_res[xkey])
+                        if local_res["nodes"] == nodes and local_res["data_seed"] == data_seed:
+                            xaxis.append(local_res["scaling_var"])
                             yaxis.append(local_res[ykey])
 
                     order = np.argsort(xaxis)
@@ -197,7 +194,7 @@ for exp in range(4):
 
                     ax.plot(xaxis[ind], yaxis[ind], color=f"C{i}", label=name)
 
-                ax.set_title(f"Data seed: {data_seed}, Model seed: {model_seed}")
+                ax.set_title(f"Data seed: {data_seed}, Nodes: {nodes}")
                 ax.set_xlabel(xkey)
                 ax.set_ylabel(ykey)
                 ax.legend()
@@ -210,7 +207,5 @@ for exp in range(4):
                     prefix = "onfly_"
                 else:
                     prefix = ""
-                # fig.savefig(save_dir / f"{prefix}data{data_seed}_model{model_seed}.png")
-                fig.savefig(save_dir / f"norm_{prefix}data{data_seed}_model{model_seed}.png")
-
-# %%
+                fig.savefig(save_dir / f"{prefix}data{data_seed}_node{nodes}.png")
+                # fig.savefig(save_dir / f"norm_{prefix}data{data_seed}_node{nodes}.png")
