05f62a93c83e6cf98eedbe243a398e315154d9d3
Vivien Cabannes <vivien.cabannes@gmail.com>
2025-01-19 10:21:27 +0000

    formatting

--- src/apps/gssm/choose_difficulty.py
+++ src/apps/gssm/choose_difficulty.py
@@ -1,9 +1,10 @@
 # %%
-import yaml
-from apps.gssm.difficulty import read_results, DifficultyEstimationConfig
-from nanollama.utils import initialize_nested_object
 import matplotlib.pyplot as plt
-import numpy as np
+import yaml
+
+from apps.gssm.difficulty import DifficultyEstimationConfig, read_results
+from nanollama.utils import initialize_nested_object
+
 
 def main() -> None:
     """
@@ -16,7 +17,6 @@ def main() -> None:
     """
     import argparse
 
-
     # parse file configuration path and task id
     parser = argparse.ArgumentParser(description=main.__doc__)
     parser.add_argument("config", type=str, help="Path to configuration file")
@@ -28,23 +28,31 @@ def main() -> None:
     # load configuration from file
     with open(path) as f:
         all_configs = yaml.safe_load(f)
-    
-    plt.figure(figsize=(15,15))
+
+    plt.figure(figsize=(15, 15))
 
     for name, config in all_configs.items():
         # initialize configuration
         config = initialize_nested_object(DifficultyEstimationConfig, config)
         print(config.path)
         hmm_estimate, gzip_estimate = read_results(config.path)
-        plt.scatter([x for x in hmm_estimate.values()], gzip_estimate.values(), alpha=.3, label=name)
-        for (alphas, h_estimate), gzip_estimate in zip(hmm_estimate.items(), gzip_estimate.values()):
+        plt.scatter([x for x in hmm_estimate.values()], gzip_estimate.values(), alpha=0.3, label=name)
+        for (alphas, h_estimate), estimate in zip(hmm_estimate.items(), gzip_estimate.values()):
             # plt.text(h_estimate, gzip_estimate, f"{alphas[0]:.2f},{alphas[1]:.2f}", ha="center", va="center", font
-            plt.annotate(f"{alphas[1]:.2e}", xy=(h_estimate, gzip_estimate), xytext=(-10, 10),
-                         textcoords="offset points", ha="right", va="bottom", arrowprops=dict(arrowstyle="->"), fontsize=5)
+            plt.annotate(
+                f"{alphas[1]:.2e}",
+                xy=(h_estimate, estimate),
+                xytext=(-10, 10),
+                textcoords="offset points",
+                ha="right",
+                va="bottom",
+                arrowprops=dict(arrowstyle="->"),
+                fontsize=5,
+            )
     plt.xlabel("H estimate")
     plt.ylabel("compression rate")
     plt.legend()
-    plt.savefig(f"compr_vs_h.png")
+    plt.savefig("compr_vs_h.png")
 
 
 if __name__ == "__main__":
--- src/apps/gssm/hidden_markov_model.py
+++ src/apps/gssm/hidden_markov_model.py
@@ -1,12 +1,13 @@
 # %%
+from functools import lru_cache
+
 import numpy as np
+import torch
 
 # from omegaconf import OmegaConf
 from nanollama.utils import initialize_nested_object
 from src.nanollama.data import gssm
-import numpy as np
-import torch
-from functools import lru_cache
+
 
 class HMM:
     SYM_IN = "abcxdefghijklmnopqrstuvwxyz"
@@ -17,17 +18,17 @@ class HMM:
         makes an HMM from a graph config via the product state
         """
         if config is not None:
-          assert top_node is None
-          config = initialize_nested_object(gssm.GSSMConfig, config, inplace=False)
-          self.rng = np.random.default_rng(random_seed)
-          self.top_node = gssm.build_gssm(config, self.rng)
+            assert top_node is None
+            config = initialize_nested_object(gssm.GSSMConfig, config, inplace=False)
+            self.rng = np.random.default_rng(random_seed)
+            self.top_node = gssm.build_gssm(config, self.rng)
         else:
-          assert top_node is not None
-          self.top_node = top_node
+            assert top_node is not None
+            self.top_node = top_node
         self.topo_order = self._dfs(self.top_node)
-        self.indexs = {node: i for i, (_,node) in enumerate(self.topo_order)}
-        self.transitions = {node: self._format_transition(node) for _,node in self.topo_order}
-    
+        self.indexs = {node: i for i, (_, node) in enumerate(self.topo_order)}
+        self.transitions = {node: self._format_transition(node) for _, node in self.topo_order}
+
     def evolve_classic(self, steps):
         for _ in range(steps):
             self.top_node.evolve()
@@ -39,8 +40,7 @@ class HMM:
             if parent.time != 0 and not parent.observed:
                 self._node_init(parent, bsz, i + 1)
         node.state = np.zeros(bsz, dtype=int)
-        assert (node.state == 0).all() # this is assumed in self.forward_probs
-
+        assert (node.state == 0).all()  # this is assumed in self.forward_probs
 
     def _init_all_nodes(self, bsz):
         self._node_init(self.top_node, bsz)
@@ -49,23 +49,22 @@ class HMM:
         def __dfs(node, fc=True):
             if not fc and node.observed:
                 return [(node.name, node)]
-            return [d for p in node.parents for d in __dfs(p, False)] + [
-                (node.name, node)
-            ]
+            return [d for p in node.parents for d in __dfs(p, False)] + [(node.name, node)]
+
         return list(dict.fromkeys(__dfs(node)))
 
     @staticmethod
     def _join_parent_kernels(node):
-      n_in = len(node.kernels)
-      # p(x|a), p(x|b) -> p(x|ab)
-      # print("individual:\n", [x.sum(-1) for x in node.kernels])
-      einsum_str = ",".join([f"{HMM.SYM_IN[i]}X" for i in range(n_in)])
-      einsum_str += "->" + "".join([HMM.SYM_IN[i] for i in range(n_in)]) + "X"
-      trans = np.einsum(einsum_str, *(np.exp(x) for x in node.kernels))
-      trans[trans.sum(-1) == 0] = 1/node.state_dim
-      trans = trans / trans.sum(-1, keepdims=True)
-      # print("prod_trans: \n", trans)
-      return trans
+        n_in = len(node.kernels)
+        # p(x|a), p(x|b) -> p(x|ab)
+        # print("individual:\n", [x.sum(-1) for x in node.kernels])
+        einsum_str = ",".join([f"{HMM.SYM_IN[i]}X" for i in range(n_in)])
+        einsum_str += "->" + "".join([HMM.SYM_IN[i] for i in range(n_in)]) + "X"
+        trans = np.einsum(einsum_str, *(np.exp(x) for x in node.kernels))
+        trans[trans.sum(-1) == 0] = 1 / node.state_dim
+        trans = trans / trans.sum(-1, keepdims=True)
+        # print("prod_trans: \n", trans)
+        return trans
 
     @staticmethod
     def _format_transition(node):
@@ -91,22 +90,15 @@ class HMM:
 
     def one_hot_product_state(self, node):
         node_order = self._dfs(node)
-        einsum_str = (
-            "B"
-            + ",B".join(HMM.SYM_IN[: len(node_order)])
-            + "->B"
-            + HMM.SYM_IN[: len(node_order)]
-        )
-        product_state = np.einsum(
-            einsum_str, *[self._one_hot_state(node) for _,node in node_order]
-        )
+        einsum_str = "B" + ",B".join(HMM.SYM_IN[: len(node_order)]) + "->B" + HMM.SYM_IN[: len(node_order)]
+        product_state = np.einsum(einsum_str, *[self._one_hot_state(node) for _, node in node_order])
         return product_state.reshape(product_state.shape[0], -1)
 
     def product_state(self, tgt_node):
         return self.one_hot_product_state(tgt_node).argmax(-1)
 
     def individual_states(self, prod_state, node_order):
-        state_dims = [node.state_dim for _,node in node_order]
+        state_dims = [node.state_dim for _, node in node_order]
         return np.unravel_index(prod_state, state_dims)
 
     @staticmethod
@@ -127,14 +119,14 @@ class HMM:
         """
         Constructs the einsum string for the target node transition matrix in the product state form
         """
-        ordered_nodes = [node for _,node in self._dfs(tgt_node)]
+        ordered_nodes = [node for _, node in self._dfs(tgt_node)]
         in_str = "".join(self._node_sym_in(n) for n in ordered_nodes)
         out_str = "".join(self._node_sym_out(n) for n in ordered_nodes)
         return in_str + out_str
 
-    @lru_cache(100)
+    @lru_cache(100)  # noqa: B019
     def make_prod_transition(self, tgt_node):
-        node_order = [node for _,node in self._dfs(tgt_node)]
+        node_order = [node for _, node in self._dfs(tgt_node)]
         state_dims = [node.state_dim for node in node_order]
         prod_input_str = HMM.SYM_IN[: len(node_order)]
 
@@ -185,7 +177,7 @@ class HMM:
         samples = torch.multinomial(torch.tensor(next_state), 1).numpy()
         return self.individual_states(samples, self.topo_order)
 
-    @lru_cache
+    @lru_cache  # noqa: B019
     def get_p_emission(self, tgt_node):
         # p_emission is a projection matrix from the product state
         # state @ p_emission = state_X
@@ -196,7 +188,7 @@ class HMM:
         shape[idx] = dim
         shape[-1] = dim
         p_emission = np.eye(dim).reshape(shape)
-        for i, (_,node) in enumerate(self.topo_order):
+        for i, (_, node) in enumerate(self.topo_order):
             shape[i] = node.state_dim
         p_emission = np.broadcast_to(p_emission, shape)
         p_emission = p_emission.reshape(-1, dim)
@@ -240,18 +232,15 @@ class HMM:
         log_p_seq = torch.zeros((1, T, B), device=device)
 
         forward_probs[:, 0, :] = log_pi[:, None] + log_E[:, obs[0]]
-        #forward_probs[:,t,:] = p(Z_t, X_[t])
-        log_p_seq[:,0,:] = lognorm(forward_probs[:, 0, :])
+        # forward_probs[:,t,:] = p(Z_t, X_[t])
+        log_p_seq[:, 0, :] = lognorm(forward_probs[:, 0, :])
         # forward_probs[:,0,:] -= log_p_seq[:,0,:]
-        
 
         log_T = log_T[:, :, None]
 
         for t in range(1, T):
-            forward_probs[:, t, :] = log_E[:, obs[t]] + torch.logsumexp(
-                forward_probs[:, None, t - 1, :] + log_T, dim=0
-            )
-            log_p_seq[:,t,:] = lognorm(forward_probs[:, t, :])
+            forward_probs[:, t, :] = log_E[:, obs[t]] + torch.logsumexp(forward_probs[:, None, t - 1, :] + log_T, dim=0)
+            log_p_seq[:, t, :] = lognorm(forward_probs[:, t, :])
             # forward_probs[:, t, :] = forward_probs[:,t,:] - log_p_seq[:,t,:]
 
         return forward_probs.cpu(), log_p_seq.reshape(T, B).cpu()
@@ -264,24 +253,22 @@ class HMM:
         transition = torch.tensor(self.make_prod_transition(self.top_node))
         emission = torch.tensor(self.get_p_emission(self.top_node))
         return self.forward_algorithm(observations, transition.log(), emission.log(), prior.log())
-    
+
     def entropy_of_observations(self, observations: np.ndarray):
         _, log_xst = self.forward_probs(observations)
         H_t = -log_xst
         H_T = H_t[-1]
         return H_T
-        
 
 
 if __name__ == "__main__":
-
-  gssm_config = {
-      "nodes": [
+    gssm_config = {
+        "nodes": [
             {
                 "name": "Z1",
                 "state_dim": 2,
                 "parents": [],
-                "alpha": .05,
+                "alpha": 0.05,
                 "mode": "default",
                 "observed": False,
             },
@@ -289,7 +276,7 @@ if __name__ == "__main__":
                 "name": "Z2",
                 "state_dim": 3,
                 "parents": ["Z1"],
-                "alpha": .05,
+                "alpha": 0.05,
                 "mode": "default",
                 "observed": False,
             },
@@ -297,7 +284,7 @@ if __name__ == "__main__":
                 "name": "Z3",
                 "state_dim": 4,
                 "parents": ["Z2"],
-                "alpha": .05,
+                "alpha": 0.05,
                 "mode": "default",
                 "observed": False,
             },
@@ -305,64 +292,65 @@ if __name__ == "__main__":
                 "name": "X",
                 "state_dim": 5,
                 "parents": ["Z1", "Z3"],
-                "alpha": .05,
+                "alpha": 0.05,
                 "mode": "default",
                 "observed": True,
             },
-      ]
-  }
+        ]
+    }
 
-  def test_prod_transition(config):
-    hmm = HMM(config, random_seed=42)
-    hmm._init_all_nodes(1000)
-    prod_transition = hmm.make_prod_transition(hmm.top_node)
-    # data_prod = hmm.fwd_product_state(prod_transition)
-    # data_prod = {name: data_prod[i] for i, name in enumerate(hmm.topo_order)}
-    data_prod_mm = hmm.fwd_via_matmul(prod_transition)
-    data_prod_mm = {name: data_prod_mm[i] for i, (name,_) in enumerate(hmm.topo_order)}
-    data_classic = hmm.evolve_classic(1)
-    import matplotlib.pyplot as plt
-    for name in data_classic:
-        plt.title(name)
-        # plt.hist(data_prod[name], label="prod", alpha=0.5)
-        plt.hist(data_prod_mm[name], label="prod_mm", alpha=0.5)
-        plt.hist(data_classic[name], label="classic", alpha=0.5)
-        plt.legend()
-        plt.show()
+    def test_prod_transition(config):
+        hmm = HMM(config, random_seed=42)
+        hmm._init_all_nodes(1000)
+        prod_transition = hmm.make_prod_transition(hmm.top_node)
+        # data_prod = hmm.fwd_product_state(prod_transition)
+        # data_prod = {name: data_prod[i] for i, name in enumerate(hmm.topo_order)}
+        data_prod_mm = hmm.fwd_via_matmul(prod_transition)
+        data_prod_mm = {name: data_prod_mm[i] for i, (name, _) in enumerate(hmm.topo_order)}
+        data_classic = hmm.evolve_classic(1)
+        import matplotlib.pyplot as plt
 
-  test_prod_transition(gssm_config)
+        for name in data_classic:
+            plt.title(name)
+            # plt.hist(data_prod[name], label="prod", alpha=0.5)
+            plt.hist(data_prod_mm[name], label="prod_mm", alpha=0.5)
+            plt.hist(data_classic[name], label="classic", alpha=0.5)
+            plt.legend()
+            plt.show()
 
-  # def test_forward_probs(config):
-  #     hmm = HMM(config)
-  #     batch_size = 2
-  #     seq_len = 2
-  #     hmm._init_all_nodes(batch_size)
-  #     observations = np.zeros((seq_len, batch_size), dtype=int)
-  #     for i in range(seq_len):
-  #         observations[i] = np.array(hmm.top_node.state)
-  #         hmm.evolve_classic(1)
-  #     #sanity check
-  #     print(hmm.forward_probs(observations)[0].exp().sum(0))
+    test_prod_transition(gssm_config)
 
-  # test_forward_probs(gssm_config)
+    # def test_forward_probs(config):
+    #     hmm = HMM(config)
+    #     batch_size = 2
+    #     seq_len = 2
+    #     hmm._init_all_nodes(batch_size)
+    #     observations = np.zeros((seq_len, batch_size), dtype=int)
+    #     for i in range(seq_len):
+    #         observations[i] = np.array(hmm.top_node.state)
+    #         hmm.evolve_classic(1)
+    #     #sanity check
+    #     print(hmm.forward_probs(observations)[0].exp().sum(0))
 
-  def test_entropy(config, seq_len, batch_size, seed=3892493):
-      observations = np.zeros((seq_len, batch_size), dtype=int)
-      n_seeds = 10
-      entropys = []
-      for i_batch in range(n_seeds):
-        mini_batch = batch_size//n_seeds
-        batch_slice = slice(i_batch*mini_batch, (i_batch+1)*mini_batch)
-        hmm = HMM(config, random_seed=seed + i_batch*1942)
-        hmm._init_all_nodes(mini_batch)
-        for i in range(seq_len):
-            observations[i,batch_slice] = np.array(hmm.top_node.state)
-            hmm.evolve_classic(1)
-        entropys.append(hmm.entropy_of_observations(observations[:,batch_slice]).mean().item())
-      return np.mean(entropys)
-  
-  for seq_len in np.logspace(0,np.log10(100),10):
-    seq_len = int(seq_len)
-    print(seq_len, test_entropy(gssm_config, seq_len, 200) / seq_len)
+    # test_forward_probs(gssm_config)
+
+    def test_entropy(config, seq_len, batch_size, seed=3892493):
+        observations = np.zeros((seq_len, batch_size), dtype=int)
+        n_seeds = 10
+        entropys = []
+        for i_batch in range(n_seeds):
+            mini_batch = batch_size // n_seeds
+            batch_slice = slice(i_batch * mini_batch, (i_batch + 1) * mini_batch)
+            hmm = HMM(config, random_seed=seed + i_batch * 1942)
+            hmm._init_all_nodes(mini_batch)
+            for i in range(seq_len):
+                observations[i, batch_slice] = np.array(hmm.top_node.state)
+                hmm.evolve_classic(1)
+            entropys.append(hmm.entropy_of_observations(observations[:, batch_slice]).mean().item())
+        return np.mean(entropys)
+
+    for seq_len in np.logspace(0, np.log10(100), 10):
+        seq_len = int(seq_len)
+        print(seq_len, test_entropy(gssm_config, seq_len, 200) / seq_len)
 
 # %%
