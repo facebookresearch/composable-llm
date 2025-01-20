2e309b7a656377caace7ab3a9978aed0a59fd718
Vivien Cabannes <vivien.cabannes@gmail.com>
2025-01-20 15:42:06 +0000

    more plots

--- exp_plots.py
+++ exp_plots.py
@@ -45,38 +45,66 @@ for exp in range(1, 5):
 
     all_data.append(merged_df)
 
-# %% Generate plots from the DataFrame
+
+# ------------------------------------------------------------------------------
+# Generate plots from the DataFrame
+# ------------------------------------------------------------------------------
+
+# %% show loss vs entropy
+
+# difficulty_key = "difficulty_hmm"
+difficulty_key = "difficulty_gzip"
+
+for data in all_data:
+    plt.figure()
+    for nb_params in data["nb_params"].unique():
+        tmp = data[data["nb_params"] == nb_params]
+        plt.scatter(tmp[difficulty_key], tmp["best"], label=f"{nb_params} params")
+    plt.xlabel(difficulty_key)
+    plt.ylabel("best loss")
+    plt.legend()
+
+# %% show loss vs alpha in the first experiment
+
+plt.figure()
+data = all_data[0]
+for index, name in zip([range(7), range(7, 13)], ["alpha_X", "alpha_Z"]):
+    mask = None
+    for i in index:
+        if mask is None:
+            mask = all_data[0]["num:data.gssm.nodes"] == i
+        else:
+            mask |= all_data[0]["num:data.gssm.nodes"] == i
+    tmp = data[mask]
+
+    for nb_params in data["nb_params"].unique():
+        _tmp = tmp[tmp["nb_params"] == nb_params]
+        plt.scatter(_tmp[difficulty_key], _tmp["best"], label=f"{nb_params} params, varying {name}")
+plt.xlabel(difficulty_key)
+plt.ylabel("best loss")
+plt.legend()
+
+# %% show loss vs graph type
+
 for exp in range(4):
     # show loss vs nb of parameters
     fig, ax = plt.subplots(1, 1, figsize=(5, 5))
     data = all_data[exp]
-    data.drop(columns=["data.gssm.nodes"], inplace=True)
     all_nodes = np.unique(data["num:data.gssm.nodes"])
+    all_params = np.unique(data["nb_params"])
 
     for node in all_nodes:
         data_node = data[data["num:data.gssm.nodes"] == node]
-        data_node = data_node.groupby(
-            [
-                "nb_params",
-            ]
-        ).mean()
-        ax.plot(data_node.index.get_level_values("nb_params"), data_node["best"], label=f"nodes={node}")
+        for nb_params in all_params[-1:]:
+            tmp = data_node[data_node["nb_params"] == nb_params]
+            ax.scatter(tmp[difficulty_key], tmp["best"], label=f"graph {node}, {nb_params} params")
 
     ax.set_xscale("log")
     ax.set_yscale("log")
     ax.set_title(f"exp={exp}")
-    ax.set_xlabel("Number of parameters")
+    ax.set_xlabel(difficulty_key)
     ax.set_ylabel("Best train loss")
     ax.legend()
     plt.show()
-
-    # show loss vs entropy
-    plt.figure()
-    for nb_params in data["nb_params"].unique():
-        tmp = data[data["nb_params"] == nb_params]
-        plt.scatter(tmp["difficulty_gzip"], tmp["best"], label=f"{nb_params} params")
-    plt.xlabel("difficulty_gzip")
-    plt.ylabel("best loss")
-    plt.legend()
 
 # %%
