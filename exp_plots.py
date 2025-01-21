85549e1a0f5022f8261d6fb8149a4821c81bbd61
Vivien Cabannes <vivien.cabannes@gmail.com>
2025-01-21 20:59:43 +0000

    nice plots

--- exp_plots.py
+++ exp_plots.py
@@ -15,22 +15,28 @@ import pandas as pd
 
 from nanollama.visualization import get_processed_results, jsonl_to_numpy, process_results
 
-HOME_DIR = Path("/checkpoint/vivc/icml/")
+LOG_DIR = Path("/checkpoint/vivc/icml/")
+CODE_DIR = Path("/private/home/vivc/code/composable-llm/")
 name = "data"
+scaling_key = "data.n_data"
+keys = [scaling_key, "grid_id"]
+
+# name = "params"
+# scaling_key = "nb_params"
+# keys = ["grid_id"]
 
 # %% Process results
-keys = ["data.n_data", "grid_id"]
 num_keys = []
 steps = [100, 300, 1000, 3000]
 
 for exp in range(1, 5):
-    log_dir = HOME_DIR / "logs" / f"exp{exp}" / name
+    log_dir = LOG_DIR / "logs" / f"exp{exp}" / name
     process_results(log_dir, keys, num_keys, steps, eval=True, copy_num=True)
 
 # %% Load processed results in a DataFrame
 all_data = []
 for exp in range(1, 5):
-    log_dir = HOME_DIR / "logs" / f"exp{exp}" / name
+    log_dir = LOG_DIR / "logs" / f"exp{exp}" / name
     data = get_processed_results(log_dir)
 
     # retrieve corresponding entropy esimates
@@ -39,9 +45,15 @@ for exp in range(1, 5):
         keys = [f"{key}_difficulty", "grid_id"]
         difficulty = pd.DataFrame(jsonl_to_numpy(filepath, keys))
         data = data.merge(difficulty, left_on=["grid_id"], right_on=["grid_id"], how="left")
-    all_data.append(data)
 
-# TODO map the data grid_id to the corresponding graph, and add this to the DataFrame (this corresponds to what was num:data.gssm.nodes before)
+    # retrieve graph information
+    prefix = CODE_DIR / "src" / "apps" / "gssm" / "configs" / f"experiment{exp}"
+    filepath = prefix / "map_grid_id_gssm_id.jsonl"
+    keys = ["grid_id", "node_id", "seed"]
+    graph_info = pd.DataFrame(jsonl_to_numpy(filepath, keys))
+    data = data.merge(graph_info, left_on=["grid_id"], right_on=["grid_id"], how="left")
+
+    all_data.append(data)
 
 # ------------------------------------------------------------------------------
 # Generate plots from the DataFrame
@@ -49,35 +61,50 @@ for exp in range(1, 5):
 
 # %% show loss vs entropy
 
-difficulty_key = "hmm_difficulty"
-# difficulty_key = "gzip_difficulty"
+# difficulty_key = "hmm_difficulty"
+difficulty_key = "gzip_difficulty"
 
-for data in all_data:
+
+for exp, data in enumerate(all_data):
     plt.figure()
     # make a line y = x
-    plt.plot([0, 3], [0, 3], color="black", ls=":")
-    for nb_params in data["nb_params"].unique():
-        tmp = data[data["nb_params"] == nb_params]
-        plt.scatter(tmp[difficulty_key], tmp["best"], label=f"{nb_params} params")
+    # plt.plot([0, 3], [0, 3], color="black", ls=":")
+    all_scales = data[scaling_key].unique()
+    nb_data = len(all_scales)
+    all_graph = data["node_id"].unique()
+    for i, grid_id in enumerate(all_graph):
+        data1 = data[data["node_id"] == grid_id]
+        for alpha, scale in enumerate(all_scales):
+            alpha = (alpha + 1) / nb_data
+            data2 = data1[data1[scaling_key] == scale]
+            plt.scatter(
+                data2[difficulty_key],
+                data2["best"],
+                label=f"{scale} {scaling_key} {grid_id}",
+                color=f"C{i}",
+                alpha=alpha,
+            )
+        # print(f"graph {grid_id} done")
     plt.xlabel(difficulty_key)
     plt.ylabel("test loss")
-    plt.legend()
+    plt.title(f"exp={exp + 1}")
+    # plt.legend()
 
-# %% (DEPRECATED - TO BE CORRECTED) show loss vs alpha in the first experiment
+# %% show loss vs alpha in the first experiment
 
 plt.figure()
 data = all_data[0]
-plt.plot([0, 3], [0, 3], color="black", ls=":")
+# plt.plot([0, 3], [0, 3], color="black", ls=":")
 for index, name in zip([range(7), range(7, 13)], ["alpha_X", "alpha_Z"]):
     mask = None
     for i in index:
         if mask is None:
-            mask = all_data[0]["num:data.gssm.nodes"] == i
+            mask = all_data[0]["node_id"] == i
         else:
-            mask |= all_data[0]["num:data.gssm.nodes"] == i
+            mask |= all_data[0]["node_id"] == i
     tmp = data[mask]
 
-    for nb_params in data["nb_params"].unique():
+    for nb_params in data["nb_params"].unique()[7:8]:
         _tmp = tmp[tmp["nb_params"] == nb_params]
         plt.scatter(_tmp[difficulty_key], _tmp["best"], label=f"{nb_params} params, varying {name}")
 plt.xlabel(difficulty_key)
@@ -90,14 +117,14 @@ for exp in range(4):
     # show loss vs nb of parameters
     fig, ax = plt.subplots(1, 1, figsize=(5, 5))
     data = all_data[exp]
-    all_nodes = np.unique(data["num:data.gssm.nodes"])
+    all_nodes = np.unique(data["node_id"])
     all_params = np.unique(data["nb_params"])
 
     for node in all_nodes:
-        data_node = data[data["num:data.gssm.nodes"] == node]
+        data_node = data[data["node_id"] == node]
         for nb_params in all_params[-1:]:
             tmp = data_node[data_node["nb_params"] == nb_params]
-            ax.scatter(tmp[difficulty_key], tmp["loss"], label=f"graph {node}, {nb_params} params")
+            ax.scatter(tmp[difficulty_key], tmp["loss_300"], label=f"graph {node}, {nb_params} params")
 
     ax.set_xscale("log")
     ax.set_yscale("log")
