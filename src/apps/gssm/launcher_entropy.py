ec03bf4fcf1962f5baa769eccb7959fe860874a4
Vivien Cabannes <vivien.cabannes@gmail.com>
2025-01-24 14:20:22 +0000

    cleaning

--- src/apps/gssm/launcher_entropy.py
+++ src/apps/gssm/launcher_entropy.py
@@ -213,15 +213,18 @@ def gzip_estimate(exp: int, code_dir: str) -> None:
 if __name__ == "__main__":
     code_dir = os.path.expandvars("$CODE_DIR")
 
-    exp = int(input("Entropy calculation. Which experiment? "))
-    print(f"Entropy calculation launch for experiment {exp}")
-    launch_entropy_estimate(exp, code_dir)
+    if input("Entropy calculation? (Y/N))").lower()[0] != "y":
+        exp = int(input("Which experiment? "))
+        print(f"Entropy calculation launch for experiment {exp}")
+        launch_entropy_estimate(exp, code_dir)
 
-    exp = int(input("Gzip calculation. Which experiment? "))
-    print(f"Running experiment {exp}")
-    gzip_estimate(exp, code_dir)
+    if input("gzip calculation? (Y/N))").lower()[0] != "y":
+        exp = int(input("Which experiment? "))
+        print(f"Running experiment {exp}")
+        gzip_estimate(exp, code_dir)
 
     # merge the hmm estimate in the same format
-    exp = int(input("Merging hmm estimate. Which experiment? "))
-    print(f"Merging hmm estimate for experiment {exp}")
-    merge_hmm_estimate(exp, code_dir)
+    if input("merging hmm calculation? (Y/N))").lower()[0] != "y":
+        exp = int(input("Which experiment? "))
+        print(f"Merging hmm estimate for experiment {exp}")
+        merge_hmm_estimate(exp, code_dir)
